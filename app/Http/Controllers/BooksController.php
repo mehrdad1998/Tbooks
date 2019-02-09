701602a0e12570ea001dd521e57fec421563d7b5
Mehrdad Dindar <mehrdad.replay@gmail.com>
2019-02-10 00:58:52 +0330

    add index And Show methode to dr controller

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Books;
 use Illuminate\Http\Request;
 
 class BooksController extends Controller
 {
-    //
+    public function index()
+    {
+        $books = Books::all();
+        return view('Index',compact('books'));
+    }
+
+    public function show($name)
+    {
+        $book = Books::where('name','=',$name)->firstOrFail();
+        return view('Show',compact('book'));
+    }
 }
